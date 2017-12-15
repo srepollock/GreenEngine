c564648717cb75e384480901b11758be7d7b7a90
Spencer Pollock <spencer@spollock.ca>
2017-12-15 14:36:08 -0800

    :smile: Engine file system refactored and cleaned up. Continuing to work on cleaning out game files, and then refactoring the system itself. Going to look at a more modular design for the system so that users can add their own files, components, objects and systems

--- GreenEngine/ResourceFiles/Shaders/shadow.vsh
+++
@@ -1,7 +0,0 @@
-#version 330 core
-layout (location = 0) in vec3 iPos;
-uniform mat4 iModelViewProjectionMatrix;
-void main()
-{
-   gl_Position = iModelViewProjectionMatrix * vec4(iPos, 1.0);
-}
---
+++ src/main.cpp
@@ -0,0 +1,61 @@
+#include "main.h" 
+#include "Engine.h"
+#include "GlobalPrefs.h"
+#include "InputEngine.h"
+#include "MessagingSystem.h"
+#include "SoundEngine.h"
+
+SDL_Window *g_window_p;
+SDL_GLContext g_context;
+std::thread* engineThread_p;
+
+/// <summary>
+/// Application entry point
+/// </summary>
+/// <param name="argc">Number of arguments passed to the application</param>
+/// <param name="argv">Array containg string arguments passed to the application</param>
+/// <return>Status code on application exit.</return>
+int main(int argc, char ** argv) {
+	SDL_Init(SDL_INIT_VIDEO);
+	InputEngine *ie = new InputEngine();
+	SoundEngine *se = new SoundEngine();
+	g_window_p = SDL_CreateWindow("RACE", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, GlobalPrefs::windowWidth, GlobalPrefs::windowHeight, SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI);
+	MessagingSystem::instance().start();
+	Engine *e = new Engine();
+	engineThread_p = e->start();
+	SoundMessageContent *content = new SoundMessageContent;
+	content->name = "Music02";
+	content->subType = S_TYPE::playMusic;
+	std::shared_ptr<Message> myMessage = std::make_shared<Message>(Message(MESSAGE_TYPE::SoundMessageType));
+	myMessage->setContent(content);
+	MessagingSystem::instance().postMessage(myMessage);
+	bool quit = false;
+	SDL_Event ev;
+	uint32_t ticksAtLast = SDL_GetTicks();
+	while (!quit)
+	{
+		SDL_PollEvent(&ev);
+		switch (ev.type)
+		{
+			case SDL_QUIT:
+				quit = true;
+				break;
+			case SDL_CONTROLLERBUTTONDOWN:
+			case SDL_CONTROLLERBUTTONUP:
+				InputButtonDownContent *content = new InputButtonDownContent();
+				content->ev = ev;
+				std::shared_ptr<Message> myMessage = std::make_shared<Message>(Message(MESSAGE_TYPE::InputButtonDownCallType));
+				myMessage->setContent(content);
+				MessagingSystem::instance().postMessage(myMessage);
+				break;
+		}
+		se->loop();
+	}
+	e->flagLoop();
+	engineThread_p->join();
+	delete(e);
+	MessagingSystem::instance().kill();
+	SDL_DestroyWindow(g_window_p);
+	delete(se);
+	return 0;
+}
